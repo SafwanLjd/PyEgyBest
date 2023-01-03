30d0911091da991ebb100635f357833eb182baec
Safwan Ljd <eljadisafwan@gamil.com>
2023-01-03 03:29:39 +0300

    minor changes and moved to host to gitlab

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
 from setuptools import setup
 
 description = 'A Selenium-less Python EgyBest Library'
-__version__ = '2.2'
+__version__ = '2.2.0'
 
 try:
 	long_description = open('README.md', 'r').read()
@@ -17,8 +19,9 @@ setup(
 	description=description,
 	long_description=long_description,
 	long_description_content_type='text/markdown',
-	url='https://github.com/SafwanLjd/PyEgyBest',
-	download_url=f'https://github.com/SafwanLjd/PyEgyBest/archive/refs/tags/v{__version__}.tar.gz',
+	python_requires='>=3.6',
+	url='https://gitlab.com/SafwanLjd/PyEgyBest',
+	download_url=f'https://gitlab.com/SafwanLjd/PyEgyBest/-/archive/v{__version__}/PyEgyBest-v{__version__}.tar.gz',
 	install_requires=['js2py', 'bs4', 'requests', 'strsimpy'],
 	classifiers=[
 		'Development Status :: 5 - Production/Stable',
@@ -26,10 +29,6 @@ setup(
 		'Topic :: Software Development :: Build Tools',
 		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 		'Natural Language :: English',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 3.6',
-		'Programming Language :: Python :: 3.7',
-		'Programming Language :: Python :: 3.8',
-		'Programming Language :: Python :: 3.9',
-	],
+		'Operating System :: OS Independent'
+	]
 )
